75d10fb8df6bb548b32bca3ef5581658b5bdc09e
Luke Vinton <luvinton@microsoft.com>
2021-01-03 18:46:32 +0000

    version up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='azure-knowledgemining-cli',
-    version=0.10,
+    version=0.91,
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/frogrammer/azure-knowledgemining-cli',
