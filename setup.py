ec8d64df026c7b986e622940620d458b00e6a823
Luke Vinton <luvinton@microsoft.com>
2021-01-06 13:32:16 +0000

    re-version

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='azure-knowledgemining-cli',
+    name='azkm',
+    description='azure knowledge mining cli',
     version="0.1.0",
     long_description=long_description,
     long_description_content_type='text/markdown',
