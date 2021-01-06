26daed6b5339816f0bf2d94f7a72ed44ba4ac2cc
Luke Vinton <luvinton@microsoft.com>
2021-01-06 11:30:38 +0000

    bugfix imagenet recipe

--- azkm/commands/recipe.py
+++ azkm/commands/recipe.py
@@ -3,7 +3,7 @@ from azkm.commands import init, deploy, dataset
 
 def deploy_imagenet(km_id: str, region:str):
     init.deploy_km(km_id, region)
-    deploy.deploy_imagenet(km_id)
+    deploy.deploy_pipeline("imagenet", km_id)
     dataset.deploy_imagenet(km_id, num_images=10)
 
 recipes = {
