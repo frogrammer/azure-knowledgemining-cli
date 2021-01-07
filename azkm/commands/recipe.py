ce3010c28cbaa15e2aba3141b717a856b9ab267c
Luke Vinton <luvinton@microsoft.com>
2021-01-07 19:35:20 +0000

    deploy app

--- azkm/commands/recipe.py
+++ azkm/commands/recipe.py
@@ -5,7 +5,8 @@ def deploy_imagenet(km_id: str, region:str):
     init.deploy_km(km_id, region)
     deploy.deploy_pipeline("imagenet", km_id)
     dataset.deploy_imagenet(km_id, num_images=10)
-
+    deploy.deploy_application("imagenet", km_id)
+    
 recipes = {
     'recipe': {
         'imagenet': deploy_imagenet
