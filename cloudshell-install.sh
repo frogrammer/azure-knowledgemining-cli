9f9d050898ca53694712a84dd0c7d973ea5659c5
Luke Vinton <luvinton@microsoft.com>
2021-01-07 23:41:52 +0000

    fix cloud shell install

--- cloudshell-install.sh
+++ cloudshell-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pip install nodeenv
 nodeenv azkm
-source ~/azkm/bin/activate
 pip install azkm
-azkm
+echo '' > ~/start_azkm.sh
+echo 'source ~/azkm/bin/activate' >  ~/start_azkm.sh
+echo 'azkm' >  ~/start_azkm.sh
+source ~/start_azkm.sh
